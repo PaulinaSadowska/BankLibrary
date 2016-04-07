06fe73c71151a76a07c108af4357dd26a8c5e90b
Paulina Sadowska <7paulina.sadowska@gmail.com>
2016-04-07 21:10:03 +0200

    InterestTest

---
+++ src/test/java/InterestTest.java
@@ -0,0 +1,49 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by Paulina Sadowska on 07.04.2016.
+ */
+public class InterestTest
+{
+    private Interest _interest;
+
+    @Before
+    public void create(){
+        _interest = new Interest(mock(IInterestCalculationStrategy.class), 0.3);
+    }
+
+    @Test
+    public void setStrategyTest() throws IllegalAccessException
+    {
+        Class<?> secretClass = _interest.getClass();
+        Field fields[] = secretClass.getDeclaredFields();
+        for (Field field : fields)
+        {
+            field.setAccessible(true);
+            if (field.getType() == IInterestCalculationStrategy.class)
+            {
+                String className = field.get(_interest).getClass().getName();
+                assertNotEquals(className, TimeDependentInterestCalculationStrategy.class.getName());
+            }
+        }
+
+        _interest.setStrategy(new TimeDependentInterestCalculationStrategy());
+
+        fields = secretClass.getDeclaredFields();
+        for (Field field : fields)
+        {
+            field.setAccessible(true);
+            if (field.getType() == IInterestCalculationStrategy.class)
+            {
+                String className = field.get(_interest).getClass().getName();
+                assertEquals(className, TimeDependentInterestCalculationStrategy.class.getName());
+            }
+        }
+    }
+}
