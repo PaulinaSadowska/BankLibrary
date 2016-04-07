80e2f095f4110437e10cbb2de70c6acc0be60257
Paulina Sadowska <7paulina.sadowska@gmail.com>
2016-04-07 18:56:51 +0200

    TimeDependentInterestCalculationStrategy tests and bug fixes

--- src/main/java/TimeDependentInterestCalculationStrategy.java
+++ src/main/java/TimeDependentInterestCalculationStrategy.java
@@ -1,13 +1,20 @@
 import java.math.BigDecimal;
+import java.security.InvalidParameterException;
+import java.util.Calendar;
 
 /**
  * Created by palka on 02.04.2016.
  */
 public class TimeDependentInterestCalculationStrategy implements IInterestCalculationStrategy {
     public BigDecimal calculateInterest(Product product, double percent) {
+        if(percent>100 || percent<0)
+            throw new InvalidParameterException();
+
         int oneDay = 1000 * 60 * 60 * 24;
-        double numOfDays = (1.0 * product.getCreationDate().getTime())/ oneDay;
-        double interest = product.getBalance().doubleValue() * numOfDays * percent;
+        long creation = product.getCreationDate().getTime();
+        long now = Calendar.getInstance().getTime().getTime();
+        long numOfDays = (now - creation)/oneDay;
+        double interest = product.getBalance().doubleValue() * numOfDays * percent * 0.01;
         return new BigDecimal(interest);
     }
 }
---
+++ src/test/java/TimeDependentInterestCalculationStrategyTest.java
@@ -0,0 +1,81 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.security.InvalidParameterException;
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by palka on 07.04.2016.
+ */
+public class TimeDependentInterestCalculationStrategyTest
+{
+    private TimeDependentInterestCalculationStrategy interestCalculationStrategy;
+    private BigDecimal _balance;
+    private Date _creationDate;
+    private Product _product;
+    private int _numOfDays;
+
+    @Before
+    public void create(){
+        interestCalculationStrategy = new TimeDependentInterestCalculationStrategy();
+        _balance = new BigDecimal(1200);
+        Calendar cal = Calendar.getInstance();
+        _numOfDays = 7;
+        cal.add(Calendar.DAY_OF_YEAR, (-1) * _numOfDays);
+        _creationDate = cal.getTime();
+
+        _product = mock(Product.class);
+        when(_product.getBalance()).thenReturn(_balance);
+        when(_product.getCreationDate()).thenReturn(_creationDate);
+    }
+
+    @Test
+    public void calculateInterestTest(){
+
+        double percent = 2;
+        double expectedInterestValue = _balance.doubleValue() * _numOfDays * percent * 0.01;
+        BigDecimal interestValue = interestCalculationStrategy.calculateInterest(_product, percent);
+        assertEquals(expectedInterestValue, interestValue.doubleValue(), 0.01);
+    }
+
+    @Test
+    public void calculateInterest_2_Test(){
+
+        double percent = 0.1;
+        double expectedInterestValue = _balance.doubleValue() * _numOfDays * percent * 0.01;
+        BigDecimal interestValue = interestCalculationStrategy.calculateInterest(_product, percent);
+        assertEquals(expectedInterestValue, interestValue.doubleValue(), 0.01);
+    }
+
+
+    @Test
+    public void calculateInterest_invalidPercent_Test(){
+
+        double percent = 101;
+        try{
+            interestCalculationStrategy.calculateInterest(_product, percent);
+            fail();
+        }
+        catch (InvalidParameterException e) {
+
+        }
+    }
+
+    @Test
+    public void calculateInterest_invalidPercent_2_Test(){
+
+        double percent = -1;
+        try{
+            interestCalculationStrategy.calculateInterest(_product, percent);
+            fail();
+        }
+        catch (InvalidParameterException e) {
+
+        }
+    }
+}
