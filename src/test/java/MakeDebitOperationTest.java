08ce0780ff4c3d48d6e711d735a5a54e1ecebd17
Paulina Sadowska <paulina.sadowska@student.put.poznan.pl>
2016-04-18 22:14:16 +0200

    MakeDebitOperationTest added

---
+++ src/test/java/MakeDebitOperationTest.java
@@ -0,0 +1,34 @@
+import Operations.MakeDebitOperation;
+import Operations.MakeLoanOperation;
+import Products.*;
+import Utils.IInterestCalculationStrategy;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+/**
+ * Created by Paulina Sadowska on 18.04.2016.
+ */
+public class MakeDebitOperationTest
+{
+    private Account _account;
+
+    @Before
+    public void setUp(){
+        _account = new Account(123, mock(BigDecimal.class), mock(Date.class), mock(Interest.class));
+    }
+
+    @Test
+    public void makeDebitToAccountTest() throws Exception
+    {
+        assertFalse(_account.hasDebit());
+        MakeDebitOperation operation = new MakeDebitOperation(_account, 1200);
+        operation.execute();
+        assertTrue(_account.hasDebit());
+    }
+
+}
